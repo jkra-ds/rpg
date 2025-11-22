af1520b7844df70047ed5880c57d0820c76ea141
jkra-ds <jkramihan03202400214@usep.edu.ph>
2025-11-23 00:01:48 +0800

    Update SaveLoad.java

--- src/io/ds/game/util/SaveLoad.java
+++ src/io/ds/game/util/SaveLoad.java
@@ -1,5 +1,13 @@
 package game.util;
+import java.util.Queue;
 
 public class SaveLoad {
-    
+
+    void save (Player player, String profileName){
+        
+    }
+
+    Player load (String profileName){
+        
+    }
 }
