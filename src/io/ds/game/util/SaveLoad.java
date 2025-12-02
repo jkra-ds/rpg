83f45946b4353c8fab47ae9f3b805a21773610f3
jkra-ds <jkramihan03202400214@usep.edu.ph>
2025-12-02 11:54:23 +0800

    Update SaveLoad.java

--- src/io/ds/game/util/SaveLoad.java
+++ src/io/ds/game/util/SaveLoad.java
@@ -1,5 +1,29 @@
 package game.util;
 
-public class SaveLoad {
-    
+import java.io.*;
+
+public static class SaveLoad {
+
+    void save (Player player, String profileName) throws IOException {
+        FileOutputStream output = new FileOutputStream(profileName);
+        ObjectOutputStream objectOutput = new ObjectOutputStream(output);
+        objectOutput.writeObject(player);
+        System.out.println("Progress Saved!");
+
+    }
+    Player load (String profileName) throws IOException, ClassNotFoundException {
+        FileInputStream input = new FileInputStream(profileName);
+        ObjectInputStream inputStream = new ObjectInputStream(input);
+        Player player = (Player) inputStream.readObject();
+        System.out.println(STR."Loading \{profileName} prpgress");
+        return player;
+    }
+
+}
+public static class Player implements Serializable{
+
+}
+
+public  static void main(String[] args) {
+
 }
